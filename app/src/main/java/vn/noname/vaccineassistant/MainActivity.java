1a5b358f20cfb37f1eff01f6845d75cfb9e06473
TRK95 <nguyetanhlanghong2@gmail.com>
2022-01-24 00:41:48 +0700

    Done user Location

--- app/src/main/java/vn/noname/vaccineassistant/MainActivity.java
+++ app/src/main/java/vn/noname/vaccineassistant/MainActivity.java
@@ -1,8 +1,16 @@
 package vn.noname.vaccineassistant;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +18,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import vn.noname.vaccineassistant.base.BaseActivity;
 import vn.noname.vaccineassistant.databinding.ActivityMainBinding;
@@ -18,6 +32,9 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMainBinding binding;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    LatLng userLatLong;
 
     @Override
     public int currentScreen() {
@@ -52,10 +69,53 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                userLatLong = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(userLatLong).title("Your Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLong, 15f));
 
-        // Add a marker in HCM city and move the camera
-        LatLng location = new LatLng(10.778545, 106.679506);
-        mMap.addMarker(new MarkerOptions().position(location).title("Marker in HCM city"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f));
+
+            }
+        };
+        // asking permission
+        askLocationPermission();
+
+    }
+
+    private void askLocationPermission() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                     return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                // get user last location to set marker
+                Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                userLatLong = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(userLatLong).title("Your Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLong, 15f));
+
+
+
+
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+
+            }
+        }).check();
     }
 }
